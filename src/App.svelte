61537dfb6d16ba7daad221b93c02a834d1761d12
Daniel <danireysan@gmail.com>
2022-05-03 17:11:45 -0500

    Created feedbackitem component and styled it

--- src/App.svelte
+++ src/App.svelte
@@ -17,10 +17,20 @@
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
   ];
+
+  $: count = feedback.length
+  $: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+
+
+const deleteFeedback = (e) => {
+  itemId = e.detail;
+  feedback = feedback.filter((item) => item.id != item.id)
+}
 </script>
 
 <main class="container">
-  <FeedbackList {feedback}/>
+  <h1>{average}</h1>
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
 
 <style>
--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -1,11 +1,18 @@
 <script>
+    import {createEventDispatcher} from "svelte";
     import Card from "./Card.svelte";
-    export let item
+    export let item;
+
+    const dispatch = createEventDispatcher()
+    const handleDelete = (itemId) => {
+        dispatch('delete-feedback', itemId)
+    }
+
 </script>
 
 <Card> 
     <div class="num-display">{item.rating}</div>
-    <div class="close">X</div>
+    <button class="close" on:click={handleDelete}>X</button>
     <p class="text-display">{item.text}</p>
 </Card>
 
@@ -30,6 +37,8 @@
         position: absolute;
         top: 10px;
         right: 20px;
+        background: none;
+        border: none;
 
     }
 </style>
