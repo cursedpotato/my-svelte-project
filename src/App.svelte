3e3e24b59d3196ae450965bf7399f43fe3d4964a
Daniel <danireysan@gmail.com>
2022-05-05 14:34:28 -0500

    created a store to handle feedback data

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,14 @@
 <script>
-  import FeedbackList from "./components/FeebackList.svelte";	
-  let feedback = [
-    {
-      id: 1,
-      rating: 10,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    },
-	{
-      id: 2,
-      rating: 8,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    },
-	{
-      id: 3,
-      rating: 7,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    },
-  ];
-
-  $: count = feedback.length
-  $: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
-
-
-const deleteFeedback = (e) => {
-  itemId = e.detail;
-  feedback = feedback.filter((item) => item.id != item.id)
-}
+  import FeedbackForm from "./components/FeedbackForm.svelte";
+  import FeedbackList from "./components/FeebackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  
 </script>
 
 <main class="container">
-  <h1>{average}</h1>
-  <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
+  <FeedbackForm/>
+  <FeedbackStats/>
+  <FeedbackList/>
 </main>
 
 <style>
---
+++ src/stores.js
@@ -0,0 +1,21 @@
+import {writable  } from "svelte/store";
+
+export const FeedbackStore = writable(
+    [
+        {
+            id: 1,
+            rating: 2,
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          },
+          {
+            id: 2,
+            rating: 8,
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          },
+          {
+            id: 3,
+            rating: 9,
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          },
+    ]
+)
